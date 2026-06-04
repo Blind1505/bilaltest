955bbbb891e3b4c05d2d193c3b7c5c592da58735
Blind1505 <blind150592@gmail.com>
2026-06-04 17:05:34 +0700

    Koneksi database sukses

---
+++ app/Controllers/TestDb.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+class TestDb extends BaseController
+{
+    public function index()
+    {
+        try {
+            $db = \Config\Database::connect();
+
+            $query = $db->query("SELECT 1");
+            echo "Koneksi database berhasil!";
+        } catch (\Exception $e) {
+            echo "<pre>";
+            echo $e->getMessage();
+            echo "</pre>";
+        }
+    }
+}
